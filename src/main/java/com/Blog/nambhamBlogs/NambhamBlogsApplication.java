e9f8dba4c7ef41c7166df27210b98cb0eebefbe4
naman <naman160234@gmail.com>
2022-01-06 23:13:02 +0530

    initial commit

---
+++ src/main/java/com/Blog/nambhamBlogs/NambhamBlogsApplication.java
@@ -0,0 +1,13 @@
+package com.Blog.nambhamBlogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NambhamBlogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NambhamBlogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Blog/nambhamBlogs/NambhamBlogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Blog.nambhamBlogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NambhamBlogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
